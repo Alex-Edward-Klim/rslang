713049f8ed0ee679082d50d0a8810fdf02c24805
Alexey Klimovskoy <Alexey_Klimovskoy@epam.com>
2021-03-23 19:37:29 +0300

    feat: implement login and redux

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import thunk from "redux-thunk";
+
+import rootReducer from "./rootReducer";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+// console.log("Initial State: ", store.getState());
+
+export default store;
---
+++ src/redux/user/userTypes.js
@@ -0,0 +1,2 @@
+// Action Types
+export const SET_USER_DATA = "SET_USER_DATA";
