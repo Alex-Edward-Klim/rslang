6e492bf4e0e08f3f93410ce6fc859274c47e1439
korytsa <anyakorytko@mail.ru>
2021-03-30 19:04:02 +0300

    feat: add burger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import styles from './header.module.css';
-import './burger-menu.css';
 import LoginStatus from '../loginStatus/LoginStatus';
-import { slide as Menu } from 'react-burger-menu';
+import burgerMenuSrc from '../../images/icon/burger-menu.svg';
+import burgerMenuCloseSrc from '../../images/icon/close-burger.svg';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+    
+    const [isBurgerMenuShow, setIsBurgerMenuShow] = useState(false);
+
     function getWindowDimensions() {
     const { innerWidth: width} = window;
     return {width};}
@@ -28,41 +32,58 @@ function Header() {
 
     const { width } = useWindowDimensions();
 
-    const menu = 
-        width >= 1182
-    ? <ul className={styles.list}>
-        <li className={styles.listItem}>Главная</li>
-        <li>Игры</li>
-        <li>Словарь</li>
-        <li>Электронный учебник</li>
-        <li>Настройки</li>
-        <li>Статистика</li>
-    </ul>
-    : <Menu>
-        <a className={styles.menuItem} href="/">
-        Главная
-        </a>
-        <a className={styles.menuItem} href="/">
-        Игры
-        </a>
-        <a className={styles.menuItem} href="/">
-        Словарь
-        </a>
-        <a className={styles.menuItem} href="/">
-        Электронный учебник
-        </a>
-        <a className={styles.menuItem} href="/">
-        Настройки
-        </a><a className={styles.menuItem} href="/">
-        Статистика
-        </a>
-    </Menu>;
+    const toggleMenu = () => {
+        setIsBurgerMenuShow(!isBurgerMenuShow)
+    }
+
+    const menuMain = (
+        <ul className={styles.list}>
+            <NavLink className={styles.listItem} to="/">Главная</NavLink>
+            <div className={styles.dropdown}>
+                <NavLink className={styles.listItem} to="/">Игры</NavLink>
+                    <ul className={styles.dropdownContent}>
+                        <NavLink className={styles.dropdownItem} to="/">Саванна</NavLink>
+                        <NavLink className={styles.dropdownItem} to="/">Аудиовызов</NavLink>
+                        <NavLink className={styles.dropdownItem} to="/">Своя игра</NavLink>
+                        <NavLink className={styles.dropdownItem} to="/">Спринт</NavLink>
+                    </ul>
+                </div>
+            <NavLink className={styles.listItem} to="/">Словарь</NavLink>
+            <NavLink className={styles.listItem} to="/">Электронный учебник</NavLink>
+            <NavLink className={styles.listItem} to="/">Настройки</NavLink>
+            <NavLink className={styles.listItem} to="/">Статистика</NavLink>
+        </ul>
+    );
     
+    const burgerMenu = (
+        <div className={styles.burgerMenuWrapper}>
+            <ul >
+                <NavLink className={styles.menuItem} to="/">Главная</NavLink>
+                <NavLink className={styles.menuItem} to="/">Игры</NavLink>
+                    <ul>
+                        <NavLink className={styles.menuItemGame} to="/">Саванна</NavLink>
+                        <NavLink className={styles.menuItemGame} to="/">Аудиовызов</NavLink>
+                        <NavLink className={styles.menuItemGame} to="/">Своя игра</NavLink>
+                        <NavLink className={styles.menuItemGame} to="/">Спринт</NavLink>
+                    </ul>
+                <NavLink className={styles.menuItem} to="/">Словарь</NavLink>
+                <NavLink className={styles.menuItem} to="/">Электронный учебник</NavLink>
+                <NavLink className={styles.menuItem} to="/">Настройки</NavLink>
+                <NavLink className={styles.menuItem} to="/">Статистика</NavLink>
+            </ul>
+            <img className={styles.burgerClose} onClick={toggleMenu} src={burgerMenuCloseSrc} alt="close" />
+        </div>
+    );
+
+    const burgerMenuImg = <img className={styles.burgerImg} onClick={toggleMenu} src={burgerMenuSrc} alt="burgerMenu" />;
+
     return (
         <header>
             <div className={styles.headerContainer}>
+                {width < 1182 ? burgerMenuImg : false}
                 <span className={styles.headerTitle}>RS Lang</span>
-                {menu}
+                {isBurgerMenuShow && burgerMenu}
+                {width >= 1182 ? menuMain : false}
                 <LoginStatus />
             </div>
         </header>
